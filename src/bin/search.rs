d53d66ac8d1233ce31efe88888de4b8cc7fcad61
Chris West (Faux) <git@goeswhere.com>
2017-03-25 12:04:14 +0000

    move trigramming to a different package; horribly inefficient and misses tails

--- src/bin/search.rs
+++ src/bin/search.rs
@@ -1,41 +1,62 @@
+#![feature(io)]
+extern crate bit_set;
 extern crate regex_syntax;
 
 use std::env;
+use std::io;
 
 use regex_syntax::Expr;
 
+type Tri = u32;
 
-fn unpack(e: &Expr) {
+mod tri;
+
+#[derive(PartialEq, Eq, Hash)]
+enum Op {
+    And(Vec<Op>),
+    Or(Vec<Op>),
+    Lit(Tri),
+}
+
+fn unpack(e: &Expr) -> Result<(), String> {
     println!("unpacking: {}", e);
     match *e {
-        Expr::Empty => println!("empty"),
         Expr::Group { ref e, i: _, name: _ } => {
             println!("group of..");
-            unpack(&e);
+            unpack(&e)
         },
         Expr::Repeat { ref e, ref r, greedy } => {
             println!("{} repeat of {} ..", greedy, r);
-            unpack(&e);
+            unpack(&e)?;
+            Err(format!("unimplemented: repeat parts: {}", e))
         },
         Expr::Concat(ref exprs) => {
             println!("{} different expressions ..", exprs.len());
             for expr in exprs {
-                unpack(expr);
+                unpack(expr)?;
             }
+            Err(format!("unimplemented: concat parts: {}", e))
         }
 
         Expr::Literal { ref chars, casei } => {
-            println!("literal: {} ({})", chars.iter().collect::<String>(), casei);
+            let lit = chars.iter().collect::<String>();
+            if casei {
+                return Err(format!("unsupported: case insensitive matching on '{}'", lit));
+            }
+            println!("literal: {} ({})", lit, casei);
+
+            Op::And(tri::trigrams_for(chars.iter().map(|c| Ok::<char, io::CharsError>(*c)))?
+                         .iter().map(|gram| Op::Lit(gram as u32)).collect());
+            Ok(())
         }
 
-        ref other => println!("unimplemented: {}", other),
-    };
-    println!("done unpacking: {}", e);
+        ref other => Err(format!("unimplemented: {}", other)),
+    }
 }
 
 fn main() {
     let regex = env::args().skip(1).next().expect("first arg: regex");
     println!("{}", regex);
     let e = Expr::parse(regex.as_str()).unwrap();
-    unpack(&e)
+    unpack(&e).unwrap()
 }
---
+++ src/bin/tri/mod.rs
@@ -0,0 +1,112 @@
+extern crate bit_set;
+
+use std::io;
+
+use bit_set::BitSet;
+
+pub type CharResult = Result<char, io::CharsError>;
+
+fn simplify(wut: char) -> u8 {
+    let c = match wut {
+        'a' ... 'z' => (wut as u8 - 'a' as u8 + 'A' as u8) as char,
+        _ => wut,
+    };
+
+    if c > 128 as char {
+        if c.is_whitespace() {
+            return 2;
+        }
+        if c.is_control() {
+            return 0;
+        }
+        return 63;
+    }
+
+    let sym_end = 33u8;
+    let letters = 21u8;
+
+    match c {
+        '\r' | '\n' => 1,
+        '\t' | '\x0c' | '\x0b' | ' ' => 2,
+        '!' => 3,
+        '"' | '\'' | '`' => 4,
+        '$' => 5,
+        '%' => 6,
+        '&' => 7,
+        '(' ... '@' => 8 + (c as u8 - '(' as u8),
+        'A' ... 'I' => sym_end + (c as u8 - 'A' as u8),
+        'J' | 'K' => sym_end + letters,
+        'L' ... 'P' => sym_end + (c as u8 - 'A' as u8) - 2,
+        'Q' => sym_end + letters,
+        'R' ... 'W' => sym_end + (c as u8 - 'A' as u8) - 3,
+        'X' | 'Z' => sym_end + letters,
+        'Y' => sym_end + letters - 1,
+        '[' => 55,
+        '\\' => 56,
+        ']'=> 57,
+        '^' | '~' | '#' => 58,
+        '_' => 59,
+        '{' ... '}' => 60 + (c as u8 - '{' as u8),
+        _ => 0,
+    }
+}
+
+fn explain(wut: u8) -> char {
+    match wut {
+        0 => 'X',
+        1 => 'N',
+        2 => ' ',
+        3 => '!',
+        4 => '"',
+        5 => '$',
+        6 => '%',
+        7 => '&',
+        8 ... 32 => (wut - 8 + '(' as u8) as char,
+        33 ... 41 => (wut - 33 + 'a' as u8) as char,
+        42 ... 46 => (wut - 42 + 'l' as u8) as char,
+        47 ... 52 => (wut - 47 + 'r' as u8) as char,
+        53 => 'y',
+        54 => 'X',
+        55 => '[',
+        56 => '\\',
+        57 => ']',
+        58 => '#',
+        59 => '_',
+        60 ... 62 => (wut - 60 + '{' as u8) as char,
+        63 => 'U',
+        _ => 'D',
+    }
+}
+
+fn unpack(wut: usize) -> String {
+    let mut ret = String::with_capacity(3);
+    ret.push(explain((wut / 64 / 64 % 64) as u8));
+    ret.push(explain((wut / 64 % 64) as u8));
+    ret.push(explain((wut % 64) as u8));
+    return ret;
+}
+
+pub fn trigrams_for<T: Iterator<Item=CharResult>>(input: T) -> Result<BitSet, String> {
+    let mut line: u64 = 1;
+    let mut prev: [u8; 3] = [0; 3];
+    let mut ret: BitSet = BitSet::with_capacity(64 * 64 * 64);
+
+    for (off, maybe_char) in input.enumerate() {
+        let c = try!(maybe_char.map_err(|e| {
+            format!("line {}: file char {}: failed: {}", line, off, e)
+        }));
+        if '\n' == c {
+            line += 1;
+        }
+        if '\0' == c {
+            return Err(format!("line {}: null found: not a text file", line));
+        }
+        prev[0] = prev[1];
+        prev[1] = prev[2];
+        prev[2] = simplify(c);
+        let tri: usize = 64 * 64 * prev[0] as usize + 64 * prev[1] as usize + prev[2] as usize;
+        ret.insert(tri);
+    }
+    return Ok(ret);
+}
+
